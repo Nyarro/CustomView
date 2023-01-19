41493816ba2121c4448786297e646e51fac268ae
Nyarro <langer360@gmail.com>
2023-01-20 00:13:06 +0100

    Initial commit

---
+++ app/src/main/java/by/sergey/customview/MainActivity.kt
@@ -0,0 +1,25 @@
+package by.sergey.customview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sadEmotionalFace = findViewById<EmotionalFace>(R.id.sadEmotionalFace)
+        val happyEmotionalFace = findViewById<EmotionalFace>(R.id.happyEmotionalFace)
+        val userEmotionalFace = findViewById<EmotionalFace>(R.id.userEmotionalFace)
+
+        happyEmotionalFace.setOnClickListener {
+            userEmotionalFace.happinessState = EmotionalFace.HAPPY
+        }
+
+        sadEmotionalFace.setOnClickListener {
+            userEmotionalFace.happinessState = EmotionalFace.SAD
+        }
+
+    }
+}
